e48931679645c02821664a22c0938ccf19d14218
Sven Trieflinger <sven.trieflinger@de.bosch.com>
2023-07-28 14:31:04 +0200

    chore: add commitlint configuration (#80)
    Signed-off-by: Sven Trieflinger <sven.trieflinger@de.bosch.com>

---
+++ commitlint.config.js
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2023 - for information on the respective copyright owner
+ * see the NOTICE file and/or the repository https://github.com/carbynestack/carbynestack.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+module.exports = {
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+      "scope-empty": [0, "never"],
+      "scope-enum": [
+        2,
+        "always",
+        ["sdk"],
+      ],
+    },
+  };
